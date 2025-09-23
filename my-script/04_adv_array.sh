3741b20a3a606e93edd003047afc0b7bf4911813
Madan Dhungel <madandhungel08@gmail.com>
2025-09-23 10:50:11 +0000

    Docs(replace and slice): learn how to replace the string and slice the string

--- my-script/04_adv_array.sh
+++ my-script/04_adv_array.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-myVar="Hello World"
+myVar="Hello my name is Madan Dhungel"
 myVarLength=${#myVar}
 echo "The length of myVar is: $myVarLength"
 echo "The length of myVar is: ${#myVar}"
@@ -14,3 +14,6 @@ echo "Lowercase is : ${myVar,,}"
 #to replace a string
 newVar=${myVar/World/Universe}
 echo "New var is : $newVar"
+
+#slice
+echo "Sliced var is : ${myVar:17:13}"
