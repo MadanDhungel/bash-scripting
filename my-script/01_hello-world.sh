984f6e5a742e7770e1c37e69060b84f8e5a9e06a
Madan Dhungel <madandhungel08@gmail.com>
2025-09-23 09:41:38 +0000

    fix(folder heirarchy): deleted some folders along with edited hello world file

---
+++ my-script/01_hello-world.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# A basic bash script example
+echo "Hello, World!"
+echo "This is a basic bash script."
+echo "Current date and time: $(date)"
+echo "Current user: $USER"
+echo "Current directory: $(pwd)"
