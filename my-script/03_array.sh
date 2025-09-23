a84a906297954594cbd91c608e825ffbfadbb0db
Madan Dhungel <madandhungel08@gmail.com>
2025-09-23 10:16:53 +0000

    Docs(specific array access): access specific value from array

--- my-script/03_array.sh
+++ my-script/03_array.sh
@@ -10,3 +10,11 @@ echo "Value in 5th index is: ${myArray[5]}"
 echo "All values in array are: ${myArray[*]}"
 #to print length of array
 echo "Length of array is: ${#myArray[*]}"
+
+#accessing specific values
+#last value suggests the increment of index 
+#i.e ${myArray[*]:2:3}:2:3 means 2nd from index and add 3 more values
+echo "The value from index 2-5 is: ${myArray[*]:2:3}"
+
+#accessing from index 3 to end
+echo "The value from index 3 to end is: ${myArray[*]:3}"
