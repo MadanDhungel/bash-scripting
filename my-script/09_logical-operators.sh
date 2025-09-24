e3cad23b2cfae89299aee2cf8bd59680452df62d
Madan Dhungel <madandhungel08@gmail.com>
2025-09-24 11:07:27 +0000

    Docs(and operator): operatio about OR  in logical operators

--- my-script/09_logical-operators.sh
+++ my-script/09_logical-operators.sh
@@ -9,3 +9,12 @@ else
     echo "You are not eligible to vote."
 fi
 
+
+#OR operator
+read -p "Enter your age: " age
+read -p "Enter your country: " country
+if  [[ $age -ge 18 || $country == "nepal" ]]; then
+    echo "You are eligible to vote."
+else
+    echo "You are not eligible to vote."
+fi
