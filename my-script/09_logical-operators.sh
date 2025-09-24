d89d6d82a54aeef801a4b91445329cb4895ca2f8
Madan Dhungel <madandhungel08@gmail.com>
2025-09-24 11:06:27 +0000

    Docs(and operator): operatio about and operator in logical operators

---
+++ my-script/09_logical-operators.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#AND operator
+read -p "Enter your age: " age
+read -p "Enter your country: " country
+if  [[ $age -ge 18 && $country == "nepal" ]]; then
+    echo "You are eligible to vote."
+else
+    echo "You are not eligible to vote."
+fi
+
