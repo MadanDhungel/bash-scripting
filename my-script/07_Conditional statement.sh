bd03d1c273d09cb1507e19b72eb806ec0871f01c
Madan Dhungel <madandhungel08@gmail.com>
2025-09-24 10:40:19 +0000

    Docs(elif):learn elif statement

--- my-script/07_Conditional statement.sh
+++ my-script/07_Conditional statement.sh
@@ -10,4 +10,15 @@ else
 fi
 
 
-#
+#elif statement
+
+read -p "Enter your marks: " marks
+if [[ $marks -ge 80 ]]; then
+    echo "You have received an A grade."
+elif [[ $marks -ge 60 ]]; then
+    echo "You have received a B grade."
+elif [[ $marks -ge 40 ]]; then
+    echo "you have received a C grade."
+else
+    echo "You have failed the exam."
+fi
